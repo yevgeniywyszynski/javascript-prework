c2f754efff1ae79a23d5c476750ff4965d4ef961
YevgeniyWyszynski <ywyszynski@gmail.com>
2021-05-14 18:44:56 +0200

    add papier

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = `kamień`;
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem'+ playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
