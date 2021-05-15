313e8909225f9570a726924f9f914fd46b945f7d
YevgeniyWyszynski <ywyszynski@gmail.com>
2021-05-16 01:28:10 +0200

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem '+ playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}else if(randomNumber == 2){
+  computerMove = 'papier';
+}else if (randomNumber == 3){
+  computerMove = 'nozyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
